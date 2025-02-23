68fafef70e6ad1dc54dfb8460430f5b1b4e68f3c
AfonsoCoelho06 <up202403260@g.uporto.pt>
2025-02-23 18:38:26 +0000

    First commit

---
+++ src/ex2ficha1_2semestre/Main.java
@@ -0,0 +1,44 @@
+package ex2ficha1_2semestre;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        Pessoa pessoa1 = new Pessoa();
+        System.out.println("Insira o nome da pessoa: ");
+        String Nomes = ler.next();
+        pessoa1.setNome(Nomes);
+
+        System.out.println("Insira a idade da pessoa: ");
+        int Idades = ler.nextInt();
+        pessoa1.setIdade(Idades);
+
+        System.out.println("Insira o peso da pessoa: ");
+        double Pesos = ler.nextDouble();
+        pessoa1.setPeso(Pesos);
+
+        System.out.println("Insira a altura da pessoa: ");
+        double Alturas = ler.nextDouble();
+        pessoa1.setAltura(Alturas);
+
+        System.out.println(pessoa1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
